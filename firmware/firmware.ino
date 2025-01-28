795b5c2dd67bb196bd7b749db0c1282e4d464d56
alticator <main@alticator.com>
2025-01-28 22:11:59 +0200

    Receive binary and send messages from client

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -2,16 +2,51 @@
 
 void setup() {
   Serial.begin(115200);
-  CanSatInit(42);
-  Serial.println();
-
+  GroundStationInit(42);
+  Serial.println("READY");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if (Serial.available() != 0) {
+    String input = Serial.readString();
+    input.trim();
+    int inputInt = input.toInt();
+    Serial.println("Changing mode to " + (String)inputInt);
+    if (inputInt >= 0 && inputInt < 3) {
+      Serial.println("Changing mode to " + (String)inputInt);
+      uint8_t dataToSend = inputInt;
+      sendData(&dataToSend, 1);
+    }
+  }
 }
 
-void onDataReceived(String data) {
-  Serial.println(data);
+
+void onBinaryDataReceived(const uint8_t *data, uint16_t len) {
+  if (len == 1) {
+    uint8_t num;
+    memcpy(&num, data, 1);
+    if (num == 42) {
+      Serial.print("PRELAUNCH:");
+      Serial.println((String)getRSSI());
+    } else if (num == 28) {
+      Serial.print("STANDBY:");
+      Serial.println((String)getRSSI());
+    } else {
+      Serial.print("UNKNOWN1");
+    }
+  } else {
+    Serial.print("LIVE:");
+    Serial.print((String)getRSSI() + ",");
+    float values[len/4];
+    for (int i = 0; i < len/4; i++) {
+      memcpy(&values[i], &data[i * 4], 4);
+      Serial.print(String(values[i], 16));
+      Serial.print(",");
+    }
+    Serial.println();
+  }
+  // } else {
+  //   Serial.println("UNKNOWN2");
+  // }
 }
